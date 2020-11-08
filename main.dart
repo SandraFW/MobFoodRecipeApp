a5b0a06d76d0ef915d1e18b592d9563a4ddb5e9b
nourghoniem <61252453+nourghoniem@users.noreply.github.com>
2020-11-08 20:43:52 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,23 +1,27 @@
-
 import 'package:flutter/material.dart';
+import 'complete_profile.dart';
+import 'trend_page.dart';
+import 'create_account.dart';
 import 'main_page.dart';
-import 'create_account.dart'
 
 void main() {
   runApp(MyApp());
 }
 
-
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+   final routes = <String, WidgetBuilder>{
+    CompleteProfile.tag: (context) => CompleteProfile(),
+    TrendPage.tag: (context) => TrendPage(), 
+  };
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       title: "Food recommendation",
       home: Create(),
+      routes: routes,
     );
+
   }
 }
-
 
