f3718ff4e4fc9ff6048350897c2f4cfe2cd66626
nourghoniem <61252453+nourghoniem@users.noreply.github.com>
2020-11-08 20:41:15 +0200

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,22 @@
+
+import 'package:flutter/material.dart';
+import 'main_page.dart';
+import 'create_account.dart'
+
+void main() {
+  runApp(MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Food recommendation",
+      home: Create(),
+    );
+  }
+}
+
+
