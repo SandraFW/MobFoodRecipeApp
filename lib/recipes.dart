7871d8b003fe9c1b1608db3720bbc5c4e2a0ef02
samihahesham <61268414+samihahesham@users.noreply.github.com>
2020-12-01 22:18:34 +0200

    Update recipes.dart

--- lib/recipes.dart
+++ lib/recipes.dart
@@ -1,158 +1,227 @@
 import 'package:flutter/material.dart';
+import 'package:like_button/like_button.dart';
+import 'one_recipe.dart';
+import 'trend_page.dart';
+import 'package:get/get.dart';
+import 'complete_profile.dart';
+import 'profile_main.dart';
 
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
+
+class FavoriteWidget extends StatefulWidget {
+  _FavoriteWidgetState createState() => _FavoriteWidgetState();
 }
 
-class _HomeState extends State<Home> {
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  bool _isFavorited = false;
+
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          child: IconButton(
+            icon: (_isFavorited
+                ? Icon(Icons.favorite)
+                : Icon(Icons.favorite_outline)),
+            color: Colors.redAccent,
+            onPressed: _toggleFavorite,
+          ),
+        ),
+      ],
+    );
+  }
+
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) {
+        _isFavorited = false;
+      } else {
+        _isFavorited = true;
+      }
+    });
+  }
+}
+
+Widget RecipesData(
+    String images, String mainText, String detailText, String rate) {
+  return Container(
+      child: Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: Container(
+            height: 289,
+            decoration: BoxDecoration(color: Colors.white, boxShadow: [
+              BoxShadow(
+                color: Colors.grey,
+              )
+            ]),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  InkWell(
+       onTap: (){
+         Get.to(OneRecipe());
+       },
+                  child:ClipRRect(
+                    borderRadius: BorderRadius.circular(8.0),
+                    child: Image.asset(
+                      images,
+                      width: 350,
+                      height: 200,
+                      fit: BoxFit.fitWidth,
+                    ),
+                  )),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        mainText,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 18),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            detailText,
+                            style: TextStyle(color: Colors.grey, fontSize: 10),
+                          ),
+                          FavoriteWidget(),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Text(rate,
+                              style:
+                                  TextStyle(fontSize: 12, color: Colors.grey)),
+                          Icon(
+                            Icons.star,
+                            color: Colors.redAccent,
+                            size: 18,
+                          ),
+                        ],
+                      ),
+                    ],
+                  )
+                ]),
+          )));
+}
+
+// ignore: camel_case_types
+class Recipes extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(
-              Icons.person,
-              color: Colors.redAccent,
-            ),
-            onPressed: () {},
-          ),
-        ],
-        title: Container(
-            margin: const EdgeInsets.only(left: 15.0, right: 15.0),
-            padding: const EdgeInsets.only(top: 10.0),
-            decoration: BoxDecoration(
-              color: Colors.grey[100],
-              borderRadius: BorderRadius.all(Radius.circular(5.0)),
-            ),
-            child: Row(
-              children: [
-                Container(
-                  margin: const EdgeInsets.only(left: 2.0, right: 15.0),
-                  padding: const EdgeInsets.only(bottom: 10.0),
+    return GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'project',
+        home: Scaffold(
+          appBar: AppBar(
+              backgroundColor: Colors.white,
+              actions: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(2.0),
+                  child: IconButton(
+                    icon: Icon(
+                      Icons.local_fire_department,
+                      color: Colors.redAccent,
+                    ),
+                    onPressed: () {
+                      Navigator.push(context,
+                        MaterialPageRoute(builder: (context) {
+                      return TrendPage();
+                    }));
+                    },
+                  ),
                 ),
-                Icon(
-                  Icons.search,
-                  color: Colors.redAccent,
-                  size: 20,
-                ),
-                Container(
-                  margin: const EdgeInsets.only(left: 2.0, right: 15.0),
-                  padding: const EdgeInsets.all(5.0),
-                ),
-                Text(
-                  'Search for a recipe...',
-                  style: TextStyle(
-                    color: Colors.grey,
-                    fontSize: 15,
+                Padding(
+                  padding: const EdgeInsets.all(2.0),
+                  child: IconButton(
+                    icon: Icon(
+                      Icons.person,
+                      color: Colors.redAccent,
+                    ),
+                    onPressed: () {
+                      Navigator.push(context,
+                        MaterialPageRoute(builder: (context) {
+                      return Profile(); 
+                    }));
+                    },
                   ),
                 ),
               ],
-            )),
-      ),
-      body: SafeArea(
-        child: ListView(children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text("5 Results",
-                    style:
-                        TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
-              ),
-              Column(
+              title: Container(
+                  margin: const EdgeInsets.only(left: 15.0, right: 15.0),
+                  padding: const EdgeInsets.only(top: 10.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      fillColor: Colors.grey[100],
+                      filled: true,
+                      hintText: "Search..",
+                      suffixIcon: Icon(
+                        Icons.search,
+                        color: Colors.redAccent,
+                        size: 20,
+                      ),
+                    ),
+                  ))),
+          body: SafeArea(
+            child: ListView(children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  IconButton(icon: Icon(Icons.filter_list), onPressed: () {}),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text("Results",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20)),
+                  ),
+                  Column(
+                    children: [
+                      IconButton(
+                          icon: Icon(Icons.filter_list), onPressed: () {}),
+                    ],
+                  )
                 ],
-              )
-            ],
+              ),
+              RecipesData(
+                  "images/roastedVegetables.jpg",
+                  "Roasted Vegetables",
+                  "A casserole dish of seasonal vegetables that is so easy to prepare.",
+                  "4.7"),
+              RecipesData("images/steak.jpg", "Steak",
+                  "Is there anything better than a well-cooked steak?", "4.2"),
+              RecipesData(
+                  "images/sashimi.jpg",
+                  "Sashimi",
+                  "Sashimi is made from fresh fish that you slice into delicate",
+                  "5.0"),
+              RecipesData(
+                  "images/shish.jpg",
+                  "Shish",
+                  " perfectly tender chicken that’s been marinating in earthy spices.",
+                  "3.9"),
+              RecipesData(
+                  "images/pizza.jpg",
+                  "Pizza",
+                  "Making pizza can be a time-consuming process, but the taste is worth.",
+                  "4.3"),
+              RecipesData(
+                  "images/pasta.jpg",
+                  "Pasta",
+                  "These baked pasta recipes are relatively inexpensive.",
+                  "4.9"),
+              RecipesData(
+                  "images/burger.jpg",
+                  "Burger",
+                  "Homemade hamburgers are a delicious treat for lunch or dinner.",
+                  "5.0"),
+              RecipesData(
+                  "images/grilledBarbecue.jpg",
+                  "Grilled Barbecue",
+                  "BBQ Chicken cooked on the grill,with your favorite barbecue sauce.",
+                  "5.0"),
+            ]),
           ),
-          Container(
-              height: 500,
-              child: ListView.builder(
-                  scrollDirection: Axis.vertical,
-                  itemCount: 5,
-                  itemBuilder: (_, index) {
-                    return Padding(
-                        padding: EdgeInsets.all(4),
-                        child: Container(
-                          height: 242,
-                          decoration:
-                              BoxDecoration(color: Colors.white, boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey,
-                            )
-                          ]),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              Image.asset(
-                                "images/food1.jpeg",
-                                height: 170,
-                                width: 310,
-                              ),
-                              Column(
-                                mainAxisAlignment: MainAxisAlignment.end,
-                                children: [
-                                  Row(children: <Widget>[
-                                    Padding(
-                                      padding: const EdgeInsets.all(1.0),
-                                      child: Text(
-                                        "Roasted Vegetables",
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 18),
-                                      ),
-                                    )
-                                  ]),
-                                  Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceBetween,
-                                      children: <Widget>[
-                                        Padding(
-                                            padding: const EdgeInsets.all(1.0),
-                                            child: Text(
-                                              "this is hands down the best way to cook vegetables.",
-                                              style: TextStyle(
-                                                  color: Colors.grey,
-                                                  fontSize: 12),
-                                            )),
-                                        Padding(
-                                            padding: const EdgeInsets.all(1.0),
-                                            child: Icon(
-                                              Icons.favorite_border,
-                                              size: 25,
-                                              color: Colors.redAccent,
-                                            )),
-                                      ]),
-                                  Row(
-                                    children: <Widget>[
-                                      Padding(
-                                        padding: const EdgeInsets.all(1.0),
-                                        child: Text('4.7',
-                                            style: TextStyle(
-                                                fontSize: 12,
-                                                color: Colors.grey)),
-                                      ),
-                                      Icon(
-                                        Icons.star,
-                                        color: Colors.redAccent,
-                                        size: 18,
-                                      )
-                                    ],
-                                  )
-                                ],
-                              )
-                            ],
-                          ),
-                        ));
-                  }))
-        ]),
-      ),
-    );
+          backgroundColor: Colors.white,
+        ));
   }
 }
