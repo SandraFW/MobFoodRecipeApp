9307cdbfea5ae37c0294295358478c9ccf678643
nourghoniem <61252453+nourghoniem@users.noreply.github.com>
2020-12-04 17:32:58 +0200

    Update intro_page.dart

--- lib/intro_page.dart
+++ lib/intro_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/animation.dart';
 import 'create_account.dart';
+import 'login.dart';
 
 class IntroPage extends StatefulWidget{
   _IntroPage createState() => _IntroPage();
@@ -9,24 +10,26 @@ class IntroPage extends StatefulWidget{
 
 class _IntroPage extends State<IntroPage> with SingleTickerProviderStateMixin{
   Animation animation;
+  Animation animationButton;
   Animation animationController;
+  Animation animationControllerButton;
+
 
   @override
   void initState(){
     super.initState();
-    animationController = AnimationController(duration: Duration(seconds: 2), vsync: this)..repeat(reverse: false);
+    animationController = AnimationController(duration: Duration(seconds: 5), vsync: this)..repeat(reverse: false);
     animation = Tween(begin: 0.0, end: 1.0).animate(CurvedAnimation(
       parent: animationController,
       curve: Curves.elasticOut,
 
     ));
-
   }
 
   @override
   void dispose() {
     super.dispose();
-    //animationController.dispose();
+    
   }
 
   Widget build (BuildContext context){
@@ -36,6 +39,7 @@ class _IntroPage extends State<IntroPage> with SingleTickerProviderStateMixin{
         body: Stack(
             children: [
             Container(
+              
             decoration: BoxDecoration(
               image: DecorationImage(
                 image: AssetImage('images/summerfruit.jpg'),
@@ -44,8 +48,9 @@ class _IntroPage extends State<IntroPage> with SingleTickerProviderStateMixin{
             ),
            
             ),
-           
-           ClipRRect(
+           Container(
+             margin: EdgeInsets.only(top: 30.0),
+           child:ClipRRect(
            child: RotationTransition( 
             turns: animation,   
             child: Image.asset(
@@ -55,53 +60,64 @@ class _IntroPage extends State<IntroPage> with SingleTickerProviderStateMixin{
            ),
          ),
             ),
+           ),
+           Container(
+          margin: EdgeInsets.only(left:330.0, top: 20.0),
+           child: TextButton(
+             onPressed: (){
+               Navigator.push(context,
+                        MaterialPageRoute(builder: (context) {
+                      return Login();
+                    }));
+             }, 
+             child: Text('Login',
+             style: TextStyle(
+               color: Colors.red[900],
+               fontWeight: FontWeight.bold,
+               fontSize: 20.5,
+             ),
+             ),
+             ),
+           ),
             Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children:[
                 Container(
-                  padding: EdgeInsets.only(left:100.0, top:510.0),
+                  padding: EdgeInsets.only(left:100.0, top:490.0),
                 ),
-                RaisedButton(
+                FlatButton(
                 onPressed: (){
                    Navigator.push(context,
                         MaterialPageRoute(builder: (context) {
                       return Create();
                     }));
-
                 },
-                  color: Colors.transparent,
-                  textColor: Colors.white,
-                  child: Container(
-                    
-                   
-                    child: Text('Sign up with email', style: TextStyle(fontSize: 20)),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(8.0)),
-                       
-                      
-
-                      ),
+              
+                  color: Colors.white.withOpacity(0.7),
+                  textColor: Colors.red[900],
+                   child: Container(
+                     child: Text(
+                     'Sign up with email', 
+                     style: TextStyle(
+                       fontSize: 20,
+                      fontWeight: FontWeight.bold)
+                     ),
+                     margin: EdgeInsets.all(18.0),
+                     padding: EdgeInsets.only(left: 30.0, right: 30.0),
+                     ),
+                    shape: new RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(18.0),
+                    side: BorderSide(color: Colors.red[900], width: 3.5 ),
                     ),
-                  
-                  ),
+                ),
+              
               ],
             ),
-                   
-                
-
-               
-
-           
             ], 
         ),
       ),
     );
 
   }
-
-
-
-
 }
