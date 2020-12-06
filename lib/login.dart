b4f958453679884fd3d4e4ffb452bda86f28b155
mariamhesham99 <61252569+mariamhesham99@users.noreply.github.com>
2020-12-06 05:14:50 +0200

    Update login.dart

--- lib/login.dart
+++ lib/login.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'constants.dart';
-
 import 'package:email_validator/email_validator.dart';
 import 'Animation/FadeAnimation.dart';
-
 import 'create_account.dart';
 import 'forgetPass.dart';
 
