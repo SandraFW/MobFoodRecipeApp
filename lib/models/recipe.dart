a0328fdf931aee8379a8111b22a04f7b6e38c59b
nourghoniem <61252453+nourghoniem@users.noreply.github.com>
2021-01-03 15:59:12 +0200

    Add files via upload

---
+++ lib/models/recipe.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:screentwo/screens/recipes.dart';
+
+class Recipe {
+  final String image;
+  final String title;
+  final String subtitle;
+  final int likes;
+
+   Recipe({this.image, this.title, this.subtitle, this.likes});
+
+}
