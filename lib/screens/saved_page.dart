ceb9d6d6c077214286d7589062890e743d3a78b9
nourghoniem <61252453+nourghoniem@users.noreply.github.com>
2021-03-09 13:15:39 +0200

    Update saved_page.dart

--- lib/screens/saved_page.dart
+++ lib/screens/saved_page.dart
@@ -1,50 +1,15 @@
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:screentwo/screens/main_page.dart';
 import 'profile_main.dart';
 import 'main_page.dart';
-import 'package:screentwo/widgets/view_recipes.dart';
-
-class SaveWidget extends StatefulWidget{
-_SaveWidgetState createState() => _SaveWidgetState();
-}
-
-class _SaveWidgetState extends State<SaveWidget> {
-bool _isSaved = true;
-
-Widget build(BuildContext context) {
-return Row(
-  
-  children: [
-   Container(
-  child: IconButton(
-  
-
-icon: (_isSaved ? Icon(Icons.turned_in) : Icon(Icons.turned_in_not_outlined)),
-color: Colors.redAccent,
-onPressed: _toggleSaved,
-),
-   ),
-  ],
-);
-
-} 
-              
-void _toggleSaved() {
-setState(() {
-if (_isSaved) {
-_isSaved = false;
-} else {
-_isSaved = true;
-}
-});
-}
-}
+import 'package:screentwo/widgets/view_saved_recipes.dart';
 
 class SavedPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-      //navigatorKey: navigatorKey,
+  
       return Scaffold(
         appBar: AppBar(
          
@@ -104,36 +69,11 @@ class SavedPage extends StatelessWidget {
         ),
 
          backgroundColor: Colors.white,
-         
-         
          body: SafeArea(
-            child: Container(
-             
-            child: ListView(
-             children: [
-               Container(
-                 margin: EdgeInsets.only(left: 30.0, top:15.0),
-               child:Text(
-              'Saved Posts',
-               style: TextStyle(
-                  fontFamily: 'SourceSansPro',
-                  fontSize: 28.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.red[900],
-                ),
-            ),
-               ),
-
-                RecipesColumn('images/crispykorean.jpg', 'Crispy Korean Fried Chicken', 'Homemade Fried Chicken',  100),
-                RecipesColumn('images/summerfruit.jpg', 'Raspberry and Peach Tart', 'Homemade Raspberry and Peach Tart', 95),
-                RecipesColumn('images/salmonwithnaan.jpg', 'Salmon with Grilled Naan', 'Homemade Salmon with Grilled Naan',  70),
-                RecipesColumn('images/pizza.jpg', 'Shrimp Pizza', 'Homemade Shrimp Pizza', 90),
-
-               ],
-                  ),
             
-           ),
-         ), 
+            child: 
+                SavedWidget(),
+         ),
     );
   }
 }
