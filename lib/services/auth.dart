43dac70e6750a3f859edad72b35c8bf6372f7975
mariamhesham99 <61252569+mariamhesham99@users.noreply.github.com>
2021-03-09 16:54:31 +0200

    Update auth.dart

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -18,7 +18,17 @@ class AuthService{
   // creating an instance from firebase authentication 
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
- 
+ Future signInWithEmailAndPassword(String email, String password) async {
+    try {
+      UserCredential result = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      User user = result.user;
+      return _userFromFirebaseUser(user);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
   Future register(String email, String password) async{
     try{
       UserCredential result = await _auth.createUserWithEmailAndPassword(email: email, password: password);
