2267b677f49861321bb38c8e80c74db3f1e3046f
SandraFW <55853937+SandraFW@users.noreply.github.com>
2021-03-10 00:27:10 +0200

    Update trend_page.dart

--- lib/screens/trend_page.dart
+++ lib/screens/trend_page.dart
@@ -1,115 +1,61 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:like_button/like_button.dart';
-import 'lib/models/Posts.dart';
+import 'package:flutter_application_6/screens/main_page.dart';
+import 'profile_main.dart';
+import 'main_page.dart';
+import 'package:flutter_application_6/widgets/view_trending_recipes.dart';
 
-
-class TrendPage extends StatefulWidget {
-  static String tag = 'trend-page';
-  @override
-  _TrendPagetState createState() => _TrendPagetState();
-}
-
-class _TrendPagetState extends State<TrendPage> {
-  //final List<Posts> _allPosts = Posts.allPosts();
-  final _saved = Set<Posts>();
-
-  //bool _isFavorited = false;
+class TrendPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-        appBar: new AppBar(
-          title: new Text(
-            "Our Popular recipes",
-            style: new TextStyle(
-                fontSize: 18.0,
-                fontWeight: FontWeight.bold,
-                color: Colors.black87),
-          ),
-        ),
-        body: new Padding(
-            padding: EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 0.0),
-            child: getHomePageBody(context)));
-  }
-
-  getHomePageBody(BuildContext context) {
-    return StreamBuilder<QuerySnapshot>(
-      stream: Firestore.instance
-          .collection('Posts')
-          .orderBy('like', descending: true)
-          .limit(30)
-          .snapshots(),
-      builder: (context, snapshot) {
-        if (!snapshot.hasData) return LinearProgressIndicator();
-
-        return _buildList(context, snapshot.data.documents);
-      },
-    );
-  }
-
-  Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
-    return ListView(
-      padding: const EdgeInsets.only(top: 20.0),
-      children: snapshot.map((data) => _buildListItem(context, data)).toList(),
-    );
-  }
-
-  Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
-    final document = Posts.fromSnapshot(data);
-    Future<bool> onLikeButtonTapped(bool isLiked) async {
-      /// send your request here
-      //final bool success= await sendRequest();
-
-      Firestore.instance.runTransaction((transaction) async {
-        final freshSnapshot = await transaction.get(document.reference);
-        final fresh = Posts.fromSnapshot(freshSnapshot);
-
-        await transaction
-            .update(document.reference, {'like': fresh.like + 1});
-      });
-
-      /// if failed, you can do nothing
-      //return success? !isLiked:isLiked;
-    }
-
-    return new Card(
-      child: new Column(
-        children: <Widget>[
-          Image.asset(
-            "images/" + document.image,
-            fit: BoxFit.fill,
-            width: 300.0,
-            height: 200.0,
-          ),
-          new Text(
-            document.title,
-            style: new TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: <Widget>[
-              LikeButton(
-                onTap: onLikeButtonTapped,
-                size: 20,
-                circleColor: CircleColor(
-                    start: Color(0xff00ddff), end: Color(0xff0099cc)),
-                bubblesColor: BubblesColor(
-                  dotPrimaryColor: Color(0xff33b5e5),
-                  dotSecondaryColor: Color(0xff0099cc),
-                ),
-                likeBuilder: (bool isLiked) {
-                  return Icon(
-                    Icons.favorite,
-                    color: isLiked ? Colors.red : Colors.grey,
-                    size: 20,
-                  );
-                },
-                likeCount: document.like,
+    return Scaffold(
+      appBar: AppBar(
+          toolbarHeight: 40,
+          backgroundColor: Colors.white,
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(
+                Icons.home,
+                color: Colors.red[900],
+                size: 25.0,
               ),
-            ],
-          ),
-        ],
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return MainPage();
+                }));
+              },
+            ),
+            IconButton(
+              icon: Icon(
+                Icons.person,
+                color: Colors.redAccent,
+              ),
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return Profile();
+                }));
+              },
+            ),
+          ],
+          title: Container(
+              margin: const EdgeInsets.only(left: 15.0, right: 15.0),
+              padding: const EdgeInsets.only(top: 5.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: "Search..",
+                  fillColor: Colors.grey[100],
+                  filled: true,
+                  suffixIcon: Icon(
+                    Icons.search,
+                    color: Colors.redAccent,
+                    size: 20,
+                  ),
+                ),
+              ))),
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: TrendWidget(),
       ),
     );
   }
 }
-
