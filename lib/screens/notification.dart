d1794033aaddc9ff9a83226d6c5f29de07fc048e
SandraFW <55853937+SandraFW@users.noreply.github.com>
2021-03-10 11:18:49 +0200

    Update notification.dart

--- lib/screens/notification.dart
+++ lib/screens/notification.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'drawer.dart';
+import 'package:flutter_application_6/widgets/drawer.dart';
+import 'package:flutter_application_6/widgets/messaging_widget.dart';
+import 'package:flutter_application_6/models/message.dart';
 
 class NotificationView extends StatelessWidget {
   @override
@@ -10,22 +12,7 @@ class NotificationView extends StatelessWidget {
         backgroundColor: Colors.redAccent,
       ),
       endDrawer: InkWellDrawer(),
-      body: ListView(
-        children: <Widget>[
-          Container(
-            padding: const EdgeInsets.only(top: 32.0),
-            child: Column(
-              children: <Widget>[
-                Padding(
-                  padding:
-                      const EdgeInsets.only(top: 22.0, left: 42.0, right: 42.0),
-                  child: Center(child: Text('No Notification to show ')),
-                )
-              ],
-            ),
-          ),
-        ],
-      ),
+      body: MessagingWidget(),
     );
   }
 }
